44833b2e6742eeb22835123fb409fce422653c2a
Gray <blackcat5523@gmail.com>
2024-09-30 14:18:44 +0800

    特色區塊文字完成

--- src/app/Featured_block.jsx
+++ src/app/Featured_block.jsx
@@ -4,6 +4,7 @@ export default function Featured (promes){
     return(
         <div className={promes.css}>
             <img src={promes.imgSrc} alt={promes.imgAlt} className={promes.imgCss}/>
+            <h3 className={promes.h5Css}>{promes.h5Text}</h3>
             <p className={promes.textCss}>{promes.textText}</p>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,44 @@ import Featured from "./Featured_block";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <NavBar title='BULEBUBBLE' navClass={styles.navbar}/>
+      <NavBar title='BULEBUBBLE' navClass={styles.navbar} />
       <main className={styles.main}>
-        <Slick/>
-        <Featured
-          css={styles.FeaturedCss}
-          imgSrc=""
-          imgAlt="1"
-          imgCss=""
-          textCss=""
-          textText="textttttts"
-        />
-      </main>
-      <footer className={styles.footer}>
+        <Slick />
+        <div>
+          <Featured
+            css={styles.FeaturedCss}
+            imgSrc=''
+            imgAlt='illustration'
+            imgCss=''
+            h5Css=''
+            h5Text='Diverse products'
+            textCss=''
+            textText='There is a diverse range of products to choose from, ranging from clothing to stationery, tableware, and even phone cases.'
+          />
+          <Featured
+            css={styles.FeaturedCss}
+            imgSrc=''
+            imgAlt='illustration'
+            imgCss=''
+            h5Css=''
+            h5Text='Customized printing'
+            textCss=''
+            textText='We offer a service where you can choose your preferred designs to be printed into finished products.'
+          />
+          <Featured
+            css={styles.FeaturedCss}
+            imgSrc=''
+            imgAlt='illustration'
+            imgCss=''
+            h5Css=''
+            h5Text='Creator Store'
+            textCss=''
+            textText='We also offer a creator store service, where you can have your own artwork printed into finished products for sale.'
+          />
+        </div>
         
-      </footer>
+      </main>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
