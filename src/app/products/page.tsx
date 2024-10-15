5b12adb0b8e370e73544769b9f8932c363304e7a
Gray <blackcat5523@gmail.com>
2024-10-15 14:31:19 +0800

    使用bootstrap做下拉選單

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,29 @@
 "use client"; //新增這行才能使用套件
 
 import NavBar from "../../api/navbar";
-import styles from "../page.module.css"; 
-export default function Products(){
+import styles from "../page.module.css";
+import DropdownMenu from "./dropdown";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-    return(
-        <>
-        <NavBar title='BLUEBUBBLE' navClass={styles.navbar} />
-        
-        </>
-    );
-
+export default function Products() {
+    
+  return (
+    <div className={styles.page}>
+      <NavBar title='BLUEBUBBLE' navClass={styles.navbar} />
+      <div className={styles.product}></div>
+      <div className={styles.main}>
+        <Container>
+          <Row>
+            <Col>
+              <DropdownMenu />
+            </Col>
+            <Col xs={8}>2 of 3 (wider)</Col>
+            <Col>3 of 3</Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
