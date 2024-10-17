f7f34d01cc87d6182044264df4a06e20b7e2f1db
Gray <blackcat5523@gmail.com>
2024-10-17 14:33:53 +0800

    修改名字

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,27 +4,20 @@ import NavBar from "../../api/navbar";
 import styles from "../page.module.css";
 
 // import DropdownMenu from "./dropdown";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Select from "./selectComponent";
+import Select from "./select-react";
 
 export default function Products() {
-    
   return (
     <div className={styles.page}>
       <NavBar title='BLUEBUBBLE' navClass={styles.navbar} />
       <div className={styles.product}></div>
       <div className={styles.main}>
-        <Container>
-          <Row>
-            <Col>
-              <Select />
-            </Col>
-            <Col xs={8}>2 of 3 (wider)</Col>
-            <Col>3 of 3</Col>
-          </Row>
-        </Container>
+        <div className={styles.searchBar}>
+          <div className={styles.selectComponent}>
+            <Select />
+          </div>
+          <input></input>
+        </div>
       </div>
     </div>
   );
