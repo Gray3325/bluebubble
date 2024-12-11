16cb81b6be9fe17742c092db623076fa2b115cd0
Gray <blackcat5523@gmail.com>
2024-12-11 15:58:53 +0800

    把特色區塊簡化了

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,24 @@
 "use client"; //新增這行才能使用套件
 import NavBar from "../components/navbar";
 import styles from "./page.module.css";
-import Carousel from "../components/carousel"
+import Carousel from "../components/carousel";
 import Featured from "../components/FeaturedBlock";
 import MultiImageCarousel from "../components/MultiImageCarousel";
-import Footer from "../components/footer"; 
+import Footer from "../components/footer";
 export default function Home() {
   return (
     <div className={styles.page}>
       {/* 導覽列 */}
-      <NavBar/> 
+      <NavBar />
       <main className={styles.main}>
         {/* 活動輪播 */}
-        <Carousel/>
+        <Carousel />
         {/* 特色區塊 */}
-        <div className={styles.FeaturedBlock}>
-          <Featured
-            imgAlt='illustration'
-            imgSrc='https://ih1.redbubble.net/image.5164288910.8373/ur,throw_pillow_couch_small,wide_portrait,600x600.jpg'
-            h5Text='Diverse products'
-            textText='There is a diverse range of products to choose from, ranging from clothing to stationery, tableware, and even phone cases.'
-          />
-          <Featured
-            imgSrc='https://ih1.redbubble.net/image.5164288910.8373/ur,throw_pillow_couch_small,wide_portrait,600x600.jpg'
-            imgAlt='illustration'
-            h5Text='Customized printing'
-            textText='We offer a service where you can choose your preferred designs to be printed into finished products.'
-          />
-          <Featured
-            imgSrc='https://ih1.redbubble.net/image.5164288910.8373/ur,throw_pillow_couch_small,wide_portrait,600x600.jpg'
-            imgAlt='illustration'
-            h5Text='Creator Store'
-            textText='We also offer a creator store service, where you can have your own artwork printed into finished products for sale.'
-          />
-        </div>
+        <Featured />
         {/* 隨機商品列 */}
-        <MultiImageCarousel/>
+        <MultiImageCarousel />
         {/* 頁尾 */}
-        <Footer/>
+        <Footer />
       </main>
     </div>
   );
--- src/components/FeaturedBlock.jsx
+++ src/components/FeaturedBlock.jsx
@@ -1,12 +1,55 @@
 import React from "react";
-import styles from "./FeaturedBlock.module.css";
+import Image from "next/image";
+import exJpg from "../app/image/ex.jpg";
 
-export default function Featured(promes) {
+const featuredDates = [
+  {
+    imgSrc: "/image/ex.jpg",
+    imgAlt: "01",
+    title: "Diverse products",
+    depiction:
+      "There is a diverse range of products to choose from, ranging from clothing to stationery, tableware, and even phone cases.",
+  },
+  {
+    imgSrc: "/image/ex.jpg",
+    imgAlt: "02",
+    title: "Customized printing",
+    depiction:
+      "We offer a service where you can choose your preferred designs to be printed into finished products.",
+  },
+  {
+    imgSrc: "/image/ex.jpg",
+    imgAlt: "03",
+    title: "Creator Store",
+    depiction:
+      "We also offer a creator store service, where you can have your own artwork printed into finished products for sale.",
+  },
+];
+
+export default function Featured() {
   return (
-    <div className={styles.FeaturedCss}>
-      <img src={promes.imgSrc} alt={promes.imgAlt} />
-      <h3>{promes.h5Text}</h3>
-      <p>{promes.textText}</p>
+    <div style={{ display: "flex" }}>
+      {featuredDates.map((featuredDate) => (
+        <div
+          key={featuredDate}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            margin: "3em",
+          }}
+        >
+          <Image
+            width={500}
+            height={500}
+            src={exJpg}
+            alt={featuredDate.imgAlt}
+            style={{width:"15em",height:"15em",borderRadius:"1em"}}
+          />
+          <h3>{featuredDate.title}</h3>
+          <p>{featuredDate.depiction}</p>
+        </div>
+      ))}
     </div>
   );
 }
